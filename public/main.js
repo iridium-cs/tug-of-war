b83c80a9ab96119dd6b62b914cdb7dcb62a42ad0
Steven Lee <info@steven-lee.com>
2018-02-14 11:29:48 -0800

    front end cleanup

--- public/main.js
+++ public/main.js
@@ -1,90 +1,78 @@
 $(function() {
-  // Create Socket
+  // variables ($ indicates jquery obj)
   var socket = io(),
-    overlay = $(".overlay"),
-    team, // a or b
+    $overlay = $(".overlay"),
+    $promptSuper = $(".super-text"),
+    $prompt = $(".prompt"),
+    $join = $("#join"),
+    $teamA = $("#teamA"),
+    $teamB = $("#teamB"),
+    $teamDiv = undefined, //assigned team div
+    team = undefined, // a or b
     joined = false,
-    teamID, //represents team div
-    prompt = $(".prompt");
+    started = false;
 
   //hide overlay at the outset
-  overlay.toggle();
+  $overlay.hide();
 
-  // Callbacks that emit info to server
+  // Events
+  $(window).keydown(function(event) {
+    if (event.keyCode === 32 && started) {
+      socket.emit("tug", team);
+      $teamDiv.fadeOut(10).fadeIn(10);
+    }
+  });
 
-  // Joins Game
-
-  function joinGame() {
+  //join game on click
+  $join.on("click", function() {
     socket.emit("newPlayer");
-    $("#join").hide("slow");
-    overlay.toggle();
-  }
+    $join.hide("slow");
+    $overlay.show();
+  });
 
-  socket.on("countdown", function(item) {
-    prompt.text(item);
-  });
-  socket.on("start", function() {
-    prompt.text("Go!");
-    overlay.toggle();
-  });
+  function resetState() {
+    $teamA.width("50%");
+    $teamB.width("50%");
+    joined = false;
+    started = false;
+    $join.show("slow");
+    socket.emit("reset");
+  }
 
   //get team assign > assign & display team/flip joined
   socket.on("teamAssign", function(response) {
     team = response.team; //teamA || teamB
     console.log(team);
-    teamID = $("#" + team);
+    $teamDiv = $("#" + team);
     joined = true;
   });
 
-  // on event countdown > display numbers (20 -> 1)
-  // on event start
-
-  // Sends a tug for given team to server
-  function tug() {
-    socket.emit("tug", team);
-  }
-
-  // Keyboard and Click events
-
-  // Sends tug on click
-  $(window).keydown(function(event) {
-    if (event.keyCode === 32) {
-      tug();
-      //flash div on press
-      teamID.fadeOut(10).fadeIn(10);
-    }
+  socket.on("countdown", function(sec) {
+    console.log(sec);
+    $promptSuper.text("You are on: " + team + ". Game starting in:");
+    $prompt.text(sec);
   });
 
-  // tries to join game on click
-  $("#join").on("click", function() {
-    joinGame();
+  socket.on("start", function() {
+    $overlay.hide();
+    started = true;
   });
-
-  // Socket events
-
-  // server socket lets us know the game is over, who won
-  socket.on("win", function() {
-    console.log("winner");
-    socket.emit("reset");
-  });
-
-  // assigns client a team
-  socket.on("joined", function() {
-    console.log("joined team");
-  });
-
-  // SUPPOSEDLY changes gameboard based on score
 
   socket.on("updateScore", function(scoreObj) {
     let totalPoints = scoreObj.teamA + scoreObj.teamB;
     let percentageA = Math.floor(100 * scoreObj.teamA / totalPoints);
     let percentageB = 100 - percentageA;
-    $("#teamA").width(percentageA + "%");
-    $("#teamB").width(percentageB + "%");
+    $teamA.width(percentageA + "%");
+    $teamB.width(percentageB + "%");
   });
 
-  socket.on("countdown", function(time) {
-    console.log(time);
+  //reset on end
+  socket.on("win", function(winner) {
+    resetState();
+    $overlay.show();
+    $promptSuper.text(winner + " won!");
+    $prompt.text("Play again!");
+    $overlay.fadeOut(1500);
   });
 
   // When anyone joins, emits "newPlayer" with {team: socket.team, numPlayers: numPlayers, teamAPlayers: teamCount[0], teamBPlayers: teamCount[1]}
